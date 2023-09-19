1bd5584f57a5653758539d33d9b8a59edae57d90
OhadEsp <ohad999@gmail.com>
2023-09-19 16:14:18 +0300

    288. Create xUnit Project and Write First Test.

---
+++ Calculator/UnitTest1.cs
@@ -0,0 +1,17 @@
+namespace Calculator
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            if (Sum(2, 2) != 4)
+                throw new Exception();
+        }
+
+        int Sum(int left, int right)
+        {
+            return left + right;
+        }
+    }
+}
