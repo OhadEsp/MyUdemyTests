8277a543c0e8e06f0b94e4085850125d91b604d2
OhadEsp <ohad999@gmail.com>
2023-09-26 07:58:13 +0300

    293. Fluent Assertions.

--- Domain/Calculator.cs
+++ Domain/Calculator.cs
@@ -5,8 +5,6 @@
 
         // 6. Got here from step 6 in UnitTest1.cs
         public int Sum(int left, int right)
-        {
-            return left + right;
-        }
+            => left + right;
     }
 }
