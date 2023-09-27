743e70dc5c7c38cec47b0a55b1202a676b2077f7
Ohad Esperansa <ohad999@gmail.com>
2023-09-27 20:47:35 +0300

    302. Parameterized Tests.

--- Flight/FlightSpecifications.cs
+++ Flight/FlightSpecifications.cs
@@ -6,20 +6,69 @@ namespace FlightTest
 {
     public class FlightSpecifications
     {
-        [Fact]
-        public void Booking_reduces_the_number_of_seats()
+        [Theory]
+        [InlineData(3,1,2)]
+        [InlineData(6,3,3)]
+        [InlineData(10,6,4)]
+        [InlineData(12,8,4)]
+        public void Booking_reduces_the_number_of_seats(int seatCapacity, int numberOfSeats, int remainingNumberOfSeats)
         {
             // 3 Clear Purpose Steps
 
             // Given
-            var flight = new Flight(seatCapacity: 3);
-            
+            var flight = new Flight(seatCapacity: seatCapacity);
+
             // When
-            flight.Book("ooo@tutorialeu.com", 1);
+            flight.Book("ooo@tutorialeu.com", numberOfSeats);
 
             // Then
-            flight.RemainingNumberOfSeats.Should().Be(2);
+            flight.RemainingNumberOfSeats.Should().Be(remainingNumberOfSeats);
         }
+
+        //[Fact]
+        //public void Booking_reduces_the_number_of_seats()
+        //{
+        //    // 3 Clear Purpose Steps
+
+        //    // Given
+        //    var flight = new Flight(seatCapacity: 3);
+            
+        //    // When
+        //    flight.Book("ooo@tutorialeu.com", 1);
+
+        //    // Then
+        //    flight.RemainingNumberOfSeats.Should().Be(2);
+        //}
+
+        //[Fact]
+        //public void Booking_reduces_the_number_of_seats_2()
+        //{
+        //    // 3 Clear Purpose Steps
+
+        //    // Given
+        //    var flight = new Flight(seatCapacity: 6);
+
+        //    // When
+        //    flight.Book("ooo@tutorialeu.com", 3);
+
+        //    // Then
+        //    flight.RemainingNumberOfSeats.Should().Be(3);
+        //}
+
+        //[Fact]
+        //public void Booking_reduces_the_number_of_seats_3()
+        //{
+        //    // 3 Clear Purpose Steps
+
+        //    // Given
+        //    var flight = new Flight(seatCapacity: 10);
+
+        //    // When
+        //    flight.Book("ooo@tutorialeu.com", 6);
+
+        //    // Then
+        //    flight.RemainingNumberOfSeats.Should().Be(4);
+        //}
 
         [Fact]
         public void Avoids_overbooking()
