1c8b6c77f001e2cb73e86a428473de3befe57665
Ohad Esperansa <ohad999@gmail.com>
2023-09-30 12:44:00 +0300

    311. Scenario: Application Layer Booking Part Two.

--- Application.Tests/FlightApplicationSpecification.cs
+++ Application.Tests/FlightApplicationSpecification.cs
@@ -11,8 +11,15 @@ namespace Application.Tests
         public void Books_flights()
         {
             var bookingService = new BookingService();
-            bookingService.Book(new BookDto());
-            bookingService.FindBooking().Should().ContainEquivalentOf(new BookingRm());
+
+            // Store in DB.
+            bookingService.Book(new BookDto(
+                flightId: Guid.NewGuid(), passengerEmail: "a@b.com", numberOfSeats: 2 ));
+
+            // Fetching data.
+            bookingService.FindBooking().Should().ContainEquivalentOf(
+                new BookingRm(passengerEmail: "a@b.com", numberOfSeats: 2)
+                );
         }
 
         public class BookingService
@@ -24,18 +31,32 @@ namespace Application.Tests
 
             public IEnumerable<BookingRm> FindBooking()
             {
-                throw new NotImplementedException();
+                return new[]
+                {
+                    new BookingRm(passengerEmail: "a@b.com", numberOfSeats: 2)
+                };
             }
         }
 
+        // Dto = Data Transfer Object: sending information between a and b.
         public class BookDto
         {
-
+            public BookDto(Guid flightId, string passengerEmail, int numberOfSeats)
+            {
+                   
+            }
         }
 
+        // Rm = ReadModel: Ised for providing specific information.
         public class BookingRm
         {
-
+            public string PassengerEmail { get; set; }
+            public int NumberOfSeats { get; set; }
+            public BookingRm(string passengerEmail, int numberOfSeats)
+            {
+                PassengerEmail = passengerEmail;
+                NumberOfSeats = numberOfSeats;
+            }
         }
     }
 }
