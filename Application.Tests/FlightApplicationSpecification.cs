2315814c6fb8540a8f4dab7bc09197163b5b92a4
Ohad Esperansa <ohad999@gmail.com>
2023-09-30 12:12:51 +0300

    310. Scenario: Application Layer Booking Part One.

--- Application.Tests/FlightApplicationSpecification.cs
+++ Application.Tests/FlightApplicationSpecification.cs
@@ -1,9 +1,39 @@
+using Xunit;
+using System.Collections.Generic;
+using System;
+using FluentAssertions;
+
 namespace Application.Tests
 {
     public class FlightApplicationSpecification
     {
         [Fact]
         public void Books_flights()
+        {
+            var bookingService = new BookingService();
+            bookingService.Book(new BookDto());
+            bookingService.FindBooking().Should().ContainEquivalentOf(new BookingRm());
+        }
+
+        public class BookingService
+        {
+            public void Book(BookDto bookDto)
+            {
+
+            }
+
+            public IEnumerable<BookingRm> FindBooking()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public class BookDto
+        {
+
+        }
+
+        public class BookingRm
         {
 
         }
